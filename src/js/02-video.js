66f4680ee72d3aa0fcfee304eda604d59215f9b4
ryiephry <ephraimpt2@gmail.com>
2023-05-21 21:42:05 -0400

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+let throttle = require('lodash.throttle');
+// on method 
+
+const onPlay = function (data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+// off method 
+
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// // If later on you decide that you don’t need to listen for play anymore.
+// player.off('play', onPlay);
+
+// // Alternatively, `off` can be called with just the event name to remove all
+// // listeners.
+// player.off('play');
+
+player.getCurrentTime().then(function (seconds) {
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
+
+player.on('play', function(data) {
+    // data is an object containing properties specific to that event
+    console.log("ji")
+});
+
+
+
+
 
