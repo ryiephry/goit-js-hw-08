018660dd4a9cf9836920be733c767f7b6de0a097
ryiephry <ephraimpt2@gmail.com>
2023-05-24 13:50:32 -0400

    test on 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
+// import throttle from "lodash.throttle";
 
+// const STORAGE_KEY_2 = "feedback-form-state"
+
+// //const input = document.getElementsByName("input");
+// //console.log(input)
+// console.log("hi")
+console.log('kk')
