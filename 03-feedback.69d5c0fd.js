12d77620c803b3b16c6a7069eba5b320a207ada3
ryiephry <116297546+ryiephry@users.noreply.github.com>
2023-05-25 15:24:44 +0000

    Deploying to gh-pages from @ ryiephry/goit-js-hw-08@b4e6e2aa80bee208c33d97ef8845527c8be64ff7 🚀

--- 03-feedback.69d5c0fd.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("feedback-form");document.getElementsByTagName("button");e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.setItem("feedback-form-state",e.target[0].value);var t=e.target[0].value;console.log(t);var o=e.target[1].value;console.log(o)})),console.log("jj")}();
-//# sourceMappingURL=03-feedback.69d5c0fd.js.map
--- 03-feedback.82058b35.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("feedback-form");document.getElementsByTagName("button");e.addEventListener("submit",(function(e){e.preventDefault(),localStorage.setItem("feedback-form-state",e.target[0].value);const t=e.target[0].value;console.log(t);const o=e.target[1].value;console.log(o)})),console.log("jj");
-//# sourceMappingURL=03-feedback.82058b35.js.map
