b4e6e2aa80bee208c33d97ef8845527c8be64ff7
ryiephry <ephraimpt2@gmail.com>
2023-05-25 11:24:14 -0400

    4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,24 +1,21 @@
 // import throttle from "lodash.throttle";
 
-const STORAGE_KEY_2 = "feedback-form-state";
+const STORAGE_KEY_2 = "feedback-form-state" ;
 
-const form = document.querySelector("feedback-form");
+const form = document.getElementsByClassName(".feedback-form");
 
-const but = document.getElementsByTagName("button");
-
+// const but = document.getElementsByTagName("button");
 
 function handelSubmit(e) {
     e.preventDefault()
     
-    localStorage.setItem(STORAGE_KEY_2, e.target[0].value);
+    localStorage.setItem(STORAGE_KEY_2 , e.target[0].value);
     const email = e.target[0].value;
     console.log(email)
     const message = e.target[1].value;
     console.log(message)
 };
 
-form.addEventListener('submit', handelSubmit)
+form.addEventListener('submit', handelSubmit);
 
 console.log("jj")
-
-
