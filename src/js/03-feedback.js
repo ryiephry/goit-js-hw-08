739faa3c805b40d02f8548a1d708d4ae48337b58
ryiephry <ephraimpt2@gmail.com>
2023-05-30 23:35:51 -0400

    email done
    need to make the local storage an object , but when i make an object it just turns it in to a string when i try to call it

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,26 @@
 // import throttle from "lodash.throttle";
 
-const STORAGE_KEY_2 = "feedback-form-state" ;
-
-const form = document.getElementsByClassName(".feedback-form");
-
-// const but = document.getElementsByTagName("button");
+const STORAGE_KEY_2 = "feedback-form-state";
+const getPreviousInput = localStorage.getItem(STORAGE_KEY_2)
+const form = document.querySelector(".feedback-form");
+form.firstElementChild.lastElementChild.value = getPreviousInput;
 
 function handelSubmit(e) {
-    e.preventDefault()
-    
-    localStorage.setItem(STORAGE_KEY_2 , e.target[0].value);
-    const email = e.target[0].value;
-    console.log(email)
+    e.preventDefault();
+
+    localStorage.setItem(STORAGE_KEY_2, e.target[0].value);
+
+    // const both = {
+    //     email: e.target[0].value,
+    //     message: e.target[1].value,
+    // };
+
+    const email   = e.target[0].value;
     const message = e.target[1].value;
+    
+    console.log(email);
     console.log(message)
+    form.reset()
 };
-
 form.addEventListener('submit', handelSubmit);
-
-console.log("jj")
+//console.log(getPreviousInput)
